4be0f6a89bd5fb5a23fd26f17715c03009fa8959
Dibyendu <dibyendusahoo03@gmail.com>
2026-01-13 20:09:03 +0530

    Design Patterns

---
+++ LLD/Design_Patterns/Creational/Prototype.py
@@ -0,0 +1,40 @@
+# The Prototype Method enables the creation of new objects by cloning existing ones, 
+# promoting efficient object creation and reducing overhead.
+# This pattern is particularly useful when the cost of creating a new object is high 
+# and when an object's initial state or configuration is complex.
+
+from abc import ABC, abstractmethod
+import copy
+
+class Prototype(ABC):
+    @abstractmethod
+    def clone(self):
+        pass
+
+class EnemyPrototype(Prototype):
+    def __init__(self, name, health, attack_power):
+        self.name = name
+        self.health = health
+        self.attack_power = attack_power
+
+    def clone(self):
+        return copy.deepcopy(self)
+
+    def __str__(self):
+        return f"Enemy(name={self.name}, health={self.health}, attack_power={self.attack_power})"
+
+def client_code(prototype: Prototype):
+    prototype_clone = prototype.clone()
+    print("Cloned enemy:", prototype_clone)
+    # Customizing the cloned enemy
+    prototype_clone.health += 10
+    prototype_clone.attack_power += 5
+    print("Customized cloned enemy:", prototype_clone)
+
+if __name__ == "__main__":
+    # Creating a prototype for a basic enemy
+    basic_enemy_prototype = EnemyPrototype("Goblin", 100, 15)
+    print("Original enemy:", basic_enemy_prototype)
+    
+    # Using the prototype to create and customize new enemies
+    client_code(basic_enemy_prototype)
